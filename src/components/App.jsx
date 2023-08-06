7e2f629ffcdd24f6319a280c512bd53137a3ca4e
Sergii Tatianchenko <taurus090@gmail.com>
2023-08-06 10:18:28 +0300

    fixed App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,80 +7,65 @@ import Loader from './loader/Loader';
 import getImages from 'api/api';
 
 const App = () => {
-  const [images, setImages] = useState([])
-  const [searchText, setSearchText] = useState('')
-  const [page, setPage] = useState(1)
-  const [modalOpen, setModalOpen] = useState(false)
-  const [imgModal, setImgModal] = useState(null)
-  const [isLoading, setIsLoading] = useState(false)
-  const [moreBtn, setMoreBtn] = useState(false)
-  
+  const [images, setImages] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [page, setPage] = useState(1);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [imgModal, setImgModal] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [moreBtn, setMoreBtn] = useState(false);
 
   useEffect(() => {
-   
-    const uploadImg = async () =>{
-      setIsLoading(true)
-      try{
-        const data = await getImages(searchText,page)
-        
-        setImages((prevState)=>([
-          ...prevState,
-          ...data.hits
-        ]))
-        setMoreBtn(page < Math.ceil(data.totalHits/12))
+    const uploadImg = async () => {
+      setIsLoading(true);
+      try {
+        const data = await getImages(searchText, page);
+
+        setImages(prevState => [...prevState, ...data.hits]);
+        setMoreBtn(page < Math.ceil(data.totalHits / 12));
       } catch (e) {
-        console.log(e)
+        console.log(e);
+      } finally {
+        setIsLoading(false);
       }
-      finally{
-        setIsLoading(false)
-      }
+    };
+    if (searchText.length > 0) {
+      uploadImg();
     }
-    if(searchText.length > 0){
-      uploadImg()
-    }
-  }, [searchText, page])
+  }, [searchText, page]);
 
-  const submitSearch = (newSearch) => {
-    setSearchText(newSearch)
+  const submitSearch = newSearch => {
+    setSearchText(newSearch);
     setImages([]);
-    setPage(1)
-    
-  }
+    setPage(1);
+  };
 
-  const handleOpenModal = (img) =>{
-    setImgModal(img)
-    setModalOpen(true)
-  }
+  const handleOpenModal = img => {
+    setImgModal(img);
+    setModalOpen(true);
+  };
 
   const handleCloseModal = () => {
-    setImgModal(null)
-    setModalOpen(false)
-  }
+    setImgModal(null);
+    setModalOpen(false);
+  };
 
   const handleLoadMore = () => {
-    setPage(page+1)
-  }
+    setPage(prevState => prevState + 1);
+  };
   return (
     <div>
-      {isLoading && <Loader/>}
-        <Searchbar submitSearch={submitSearch} />
-        {images.length ? (
-          <ImageGallery
-            handleOpenModal={handleOpenModal}
-            imagesStore={images}
-          />
-        ) : null}
-        {moreBtn && (
-          <Button handleLoadMore={handleLoadMore} />
-        ) }
-        {modalOpen && (
-          <Modal
-            imgModal= {imgModal}
-            handleCloseModal={handleCloseModal}
-          />
-        )}
+      {isLoading && <Loader />}
+      <Searchbar submitSearch={submitSearch} />
+      {images.length > 0 && (
+        <ImageGallery handleOpenModal={handleOpenModal} imagesStore={images} />
+      )}
+      {moreBtn && <Button handleLoadMore={handleLoadMore} />}
+      {modalOpen && (
+        <Modal imgModal={imgModal} handleCloseModal={handleCloseModal} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
